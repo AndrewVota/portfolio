99695985eb9d0bd9c95ba7ff6df9dca14a36734a
Andrew Vota <andrewvota@gmail.com>
2023-12-26 12:07:44 -0500

    feat: Markdown layout and styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [UnoCSS()],
-	trailingSlash: "always",
-	site: "https://andrewvota.dev",
-	output: "static"
+  integrations: [UnoCSS(), mdx()],
+  trailingSlash: "always",
+  site: "https://andrewvota.dev",
+  output: "static"
 });
